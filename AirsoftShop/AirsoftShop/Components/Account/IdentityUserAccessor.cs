45ed79039141e311ac9acdd4432938e1ac9ed6c9
Arkadiusz Zwolenik <arek.zwolenik@o2.pl>
2024-11-04 12:38:10 +0100

    Source Files

---
+++ AirsoftShop/AirsoftShop/Components/Account/IdentityUserAccessor.cs
@@ -0,0 +1,20 @@
+using AirsoftShop.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace AirsoftShop.Components.Account
+{
+    internal sealed class IdentityUserAccessor(UserManager<ApplicationUser> userManager, IdentityRedirectManager redirectManager)
+    {
+        public async Task<ApplicationUser> GetRequiredUserAsync(HttpContext context)
+        {
+            var user = await userManager.GetUserAsync(context.User);
+
+            if (user is null)
+            {
+                redirectManager.RedirectToWithStatus("Account/InvalidUser", $"Error: Unable to load user with ID '{userManager.GetUserId(context.User)}'.", context);
+            }
+
+            return user;
+        }
+    }
+}
