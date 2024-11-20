5acfc29449da46a7c3dde6b76ee4911aec206fb9
Arkadiusz Zwolenik <98668031+Aro3224@users.noreply.github.com>
2024-11-20 09:14:05 +0100

    bazowy kontekst dla dzialania bazy

--- AirsoftShop/AirsoftShop/Data/ApplicationDbContext.cs
+++ AirsoftShop/AirsoftShop/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -5,5 +6,31 @@ namespace AirsoftShop.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
     {
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            var password = new PasswordHasher<ApplicationUser>();
+            var hashed = password.HashPassword(new ApplicationUser(), "Admin1!");
+
+            modelBuilder.Entity<ApplicationUser>().HasData(
+                new ApplicationUser() { Id = "1", UserName = "admin1", NormalizedUserName = "ADMIN1", PasswordHash = hashed, EmailConfirmed = true, Email = "admin@gmail.com", NormalizedEmail = "ADMIN@GMAIL.COM", LockoutEnabled = true },
+                new ApplicationUser() { Id = "2", UserName = "user1", NormalizedUserName = "USER1", PasswordHash = hashed, EmailConfirmed = true, Email = "user@gmail.com", NormalizedEmail = "USER@GMAIL.COM", LockoutEnabled = true },
+                new ApplicationUser() { Id = "3", UserName = "user2", NormalizedUserName = "USER2", PasswordHash = hashed, EmailConfirmed = true, Email = "user2@gmail.com", NormalizedEmail = "USER2@GMAIL.COM", LockoutEnabled = true },
+                new ApplicationUser() { Id = "4", UserName = "user3", NormalizedUserName = "USER3", PasswordHash = hashed, EmailConfirmed = true, Email = "user3@gmail.com", NormalizedEmail = "USER3@GMAIL.COM", LockoutEnabled = true }
+                );
+
+            modelBuilder.Entity<IdentityRole>().HasData(
+                new IdentityRole() { Id = "1", Name = "Admin", NormalizedName = "ADMIN" },
+                new IdentityRole() { Id = "2", Name = "User", NormalizedName = "USER" }
+                );
+
+            modelBuilder.Entity<IdentityUserRole<string>>().HasData(
+                new IdentityUserRole<string> { UserId = "1", RoleId = "1" },
+                new IdentityUserRole<string> { UserId = "2", RoleId = "2" },
+                new IdentityUserRole<string> { UserId = "3", RoleId = "2" },
+                new IdentityUserRole<string> { UserId = "4", RoleId = "2" }
+                );
+        }
     }
 }
