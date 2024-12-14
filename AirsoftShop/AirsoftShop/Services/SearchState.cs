9d23a8c6b14ae38fb1e6ea05425c64a765195e34
Arkadiusz Zwolenik <98668031+Aro3224@users.noreply.github.com>
2024-12-14 15:17:57 +0100

    Wyszukiwanie itemów - główna strona
    Jeśli user jest w jakiejś kategorii przedmiotów to wyszukiwanie nie działa

---
+++ AirsoftShop/AirsoftShop/Services/SearchState.cs
@@ -0,0 +1,15 @@
+﻿namespace AirsoftShop.Services
+{
+    public class SearchState
+    {
+        public string Query { get; private set; } = string.Empty;
+
+        public event Action? OnSearchChanged;
+
+        public void SetSearchQuery(string query)
+        {
+            Query = query;
+            OnSearchChanged?.Invoke();
+        }
+    }
+}
