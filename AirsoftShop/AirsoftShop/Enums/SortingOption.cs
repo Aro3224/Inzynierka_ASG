c007b1908e45eaa1544c1886704406b9d79618e2
Arkadiusz Zwolenik <98668031+Aro3224@users.noreply.github.com>
2024-12-18 12:16:05 +0100

    Sortowanie replik

---
+++ AirsoftShop/AirsoftShop/Enums/SortingOption.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AirsoftShop.Enums
+{
+    public enum SortingOption
+    {
+        [Display(Name = "Domyślne")]
+        None,
+
+        [Display(Name = "Cena rosnąco")]
+        PriceAsc,
+
+        [Display(Name = "Cena malejąco")]
+        PriceDesc,
+
+        [Display(Name = "Nazwa A-Z")]
+        NameAsc,
+
+        [Display(Name = "Nazwa Z-A")]
+        NameDesc
+    }
+}
---
+++ AirsoftShop/AirsoftShop/Services/SortingState.cs
@@ -0,0 +1,33 @@
+﻿using AirsoftShop.Enums;
+
+namespace AirsoftShop.Services
+{
+    public class SortingState
+    {
+        public SortingOption CurrentSortingOption { get; private set; } = SortingOption.None;
+
+        public event Action? OnSortingChanged;
+
+        public void SetSortingOption(SortingOption sortingOption)
+        {
+            CurrentSortingOption = sortingOption;
+            OnSortingChanged?.Invoke();
+        }
+
+        public void SetSortingOptionFromUrl(Uri uri)
+        {
+            var sorting = System.Web.HttpUtility.ParseQueryString(uri.Query).Get("sortBy") ?? string.Empty;
+
+            if (Enum.TryParse(sorting, true, out SortingOption parsedOption))
+            {
+                CurrentSortingOption = parsedOption;
+            }
+            else
+            {
+                CurrentSortingOption = SortingOption.None;
+            }
+
+            OnSortingChanged?.Invoke();
+        }
+    }
+}
