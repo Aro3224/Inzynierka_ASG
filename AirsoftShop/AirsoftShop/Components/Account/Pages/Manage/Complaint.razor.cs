65ee96872aee6078cfb9925f66c7e81028180be1
Arkadiusz Zwolenik <98668031+Aro3224@users.noreply.github.com>
2025-01-08 12:13:59 +0100

    Wczytanie itemów z orderu

--- AirsoftShop/AirsoftShop/Components/Account/Pages/Manage/Complaint.razor.cs
+++ AirsoftShop/AirsoftShop/Components/Account/Pages/Manage/Complaint.razor.cs
@@ -1,4 +1,8 @@
+using AirsoftShop.Data;
+using AirsoftShop.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Identity;
 
 namespace AirsoftShop.Components.Account.Pages.Manage
 {
@@ -6,5 +10,41 @@ namespace AirsoftShop.Components.Account.Pages.Manage
     {
         [Parameter]
         public required int OrderId { get; set; }
+
+        public required Order Order { get; set; }
+
+        private bool IsAuthorized { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            var authState = await AuthenticationStateProvider.GetAuthenticationStateAsync();
+            var user = authState.User;
+
+            if (user.Identity?.IsAuthenticated != true)
+            {
+                IsAuthorized = false;
+                return;
+            }
+
+            var currentUser = await UserManager.GetUserAsync(user);
+            if (currentUser is null)
+            {
+                IsAuthorized = false;
+                return;
+            }
+
+            Order = await OrderService.GetOrderByIdAsync(OrderId);
+            if (Order is not null && Order.UserId == currentUser.Id)
+            {
+                IsAuthorized = true;
+            }
+            else
+            {
+                IsAuthorized = false;
+                Order = null;
+            }
+        }
+
+
     }
 }
