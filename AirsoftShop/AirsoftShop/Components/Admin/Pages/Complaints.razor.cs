04b9903079a68ef7e5e134f32ea8334eb8ee805d
Arkadiusz Zwolenik <98668031+Aro3224@users.noreply.github.com>
2025-01-10 15:13:49 +0100

    e-mail w zgłoszeniach

--- AirsoftShop/AirsoftShop/Components/Admin/Pages/Complaints.razor.cs
+++ AirsoftShop/AirsoftShop/Components/Admin/Pages/Complaints.razor.cs
@@ -1,12 +1,20 @@
+using AirsoftShop.Services;
+
 namespace AirsoftShop.Components.Admin.Pages
 {
     public partial class Complaints
     {
         public required List<Data.Complaint> ComplaintList { get; set; }
 
+        private Dictionary<string, string> UserEmails = new();
+
         protected override async Task OnInitializedAsync()
         {
             ComplaintList = await ComplaintService.GetComplaintsAsync();
+
+            var allUsers = await UserService.GetAllUsersAsync();
+
+            UserEmails = allUsers.ToDictionary(u => u.Id, u => u.Email);
         }
     }
 }
--- AirsoftShop/AirsoftShop/Components/Admin/Pages/ServiceRequests.razor.cs
+++ AirsoftShop/AirsoftShop/Components/Admin/Pages/ServiceRequests.razor.cs
@@ -6,9 +6,15 @@ namespace AirsoftShop.Components.Admin.Pages
     {
         public required List<PostWarranty> RequestsList { get; set; }
 
+        private Dictionary<string, string> UserEmails = new();
+
         protected override async Task OnInitializedAsync()
         {
             RequestsList = await PostWarrantyService.GetPostWarrantiesAsync();
+
+            var allUsers = await UserService.GetAllUsersAsync();
+
+            UserEmails = allUsers.ToDictionary(u => u.Id, u => u.Email);
         }
     }
 }
